d7646e9954a90b4c1bd3935ee0e7969533552cc0
Met4physics <91643087+Met4physics@users.noreply.github.com>
2022-07-26 22:43:34 +0800

    Add files via upload

---
+++ lab00.py
@@ -0,0 +1,10 @@
+def twenty_twenty():
+    """Come up with the most creative expression that evaluates to 2020,
+    using only numbers and the +, *, and - operators.
+
+    >>> twenty_twenty()
+    2020
+    """
+    a = 1010 * (3-1)
+    return a
+
